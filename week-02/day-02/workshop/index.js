84ce53261f163864ee072883204ae3f3c2df0a67
Jialu Gu <msljtacslw@gmail.com>
2019-10-22 15:45:33 +0800

    feat(workshop): add "Vector initializer

--- week-02/day-02/workshop/index.js
+++ week-02/day-02/workshop/index.js
@@ -319,3 +319,27 @@ function BMI(BMIObj){
 }
 
 console.log(BMI(BMIObj));
+
+
+//Vector initializer
+const vectors = [
+  [1, 2, 3],
+  [],
+  [2, 3],
+  [-1],
+  [6, 7, 8, 9],
+];
+// expected result:
+// [1,2,3]
+// [0,0,0]
+// [2,3,0]
+// [-1,0,0]
+// [6,7,8]
+
+function vectorInit(x=0, y=0, z=0){
+  return [x, y, z];
+}
+
+for(let vector of vectors){
+  console.log(vectorInit(...vector));
+}
