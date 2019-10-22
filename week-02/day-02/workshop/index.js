ac990ba489a659796ae0b19dfccbbc6d32eb448e
Jialu Gu <msljtacslw@gmail.com>
2019-10-22 11:45:35 +0800

    feat(workshop): Add Classes

--- week-02/day-02/workshop/index.js
+++ week-02/day-02/workshop/index.js
@@ -1,7 +1,16 @@
 //Arrow function
+/*
 function multiPurposeFunction(action) {
   if (action) {
     console.log(action(3));
+  }
+}
+*/
+
+function multiPurposeFunction(action) {
+  if (action) {
+    console.log(action('John', 'Smith'));
+    console.log(action('', 'Bond'));
   }
 }
 
@@ -10,6 +19,7 @@ function exampleNonAnonymFunction(param) {
 }
 
 function frameFunction() {
+  /*
   // this function call shows an example,
   // but it is not using anonym functions
   multiPurposeFunction(exampleNonAnonymFunction);
@@ -17,6 +27,51 @@ function frameFunction() {
 
   const anoymFunction = (x) => x*x;
   multiPurposeFunction(anoymFunction);
+  */
+
+  const anoymFunction = (a, b) => a === '' ? b : a + ' ' + b;
+  multiPurposeFunction(anoymFunction);
 }
 
 frameFunction();
+
+
+//Classes
+function Garden(width, length){
+  this.width = width;
+  this.length = length;
+}
+
+Garden.prototype.area = function(){
+  return this.width*this.length;
+}
+
+Garden.prototype.circumference = function(){
+  return this.width*2 + this.length*2;
+}
+
+Garden.prototype.efficiency = function(){
+  return this.area()/this.circumference();
+}
+
+class GardenES6{
+  constructor(width, length){
+    this.width = width;
+    this.length = length;
+  }
+
+  area(){
+    return this.width*this.length;
+  }
+
+
+  circumference(){
+    return this.width*2 + this.length*2;
+  }
+
+  efficiency(){
+    return this.area()/this.circumference();
+  }
+
+}
+
