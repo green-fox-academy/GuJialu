07dd921628ff7e21fe4101d278f2b1e5f1a8c36f
Jialu Gu <msljtacslw@gmail.com>
2019-10-23 16:39:52 +0800

    feat(workshop): add stack and queue

---
+++ week-02/day-03/workshop/StackAndQueue.ts
@@ -0,0 +1,51 @@
+export interface Stack {
+  empty(): boolean;
+  peek(): string;
+  push(value: string): void;
+  pop(): string;
+}
+
+export class myStack implements Stack {
+
+  stack: string[];
+
+  empty(): boolean {
+    return this.stack.length === 0;
+  }
+  peek(): string {
+    return this.stack[this.stack.length-1];
+  }
+  push(value: string): void {
+    this.stack.push(value);
+  }
+  pop(): string {
+    return this.stack.pop();
+  }
+
+}
+
+interface Queue {
+  empty(): boolean;
+  peek(): string;
+  add(value: string): void;
+  remove(): string;
+}
+
+class myQueue implements Queue{
+
+  queue: string[]
+
+  empty(): boolean {
+    return this.queue.length === 0;
+  }
+  peek(): string {
+    return this.queue[this.queue.length-1];
+  }
+  add(value: string): void {
+    this.queue.push(value);
+  }
+  remove(): string {
+    return this.queue.shift();
+  }
+
+}
