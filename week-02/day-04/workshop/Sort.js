a9f1a5f4c6fec7dada10bc406cbd097ccfe5aaf5
Jialu Gu <msljtacslw@gmail.com>
2019-10-24 12:00:22 +0800

    style(workshop): follow style guide in bubbleSort and MerageSort

--- week-02/day-04/workshop/Sort.js
+++ week-02/day-04/workshop/Sort.js
@@ -1,11 +1,11 @@
 function BubbleSort(a) {
 
-  for(let i=0; i<a.length; i++){
-    for(let j=0; j+1<a.length; j++){
-      if(a[j]>a[j+1]){
+  for (let i = 0; i < a.length; i++) {
+    for (let j = 0; j + 1 < a.length; j++) {
+      if (a[j] > a[j + 1]) {
         let temp = a[j];
-        a[j] = a[j+1];
-        a[j+1] = temp;
+        a[j] = a[j + 1];
+        a[j + 1] = temp;
       }
     }
   }
@@ -14,19 +14,19 @@ function BubbleSort(a) {
 
 function MerageSort(a) {
 
-  if(a.length===1){
+  if (a.length === 1) {
     return a;
   }
 
-  let a1 = MerageSort(a.slice(0, a.length/2));
-  let a2 = MerageSort(a.slice(a.length/2, a.length));
-  let i=0, j=0;
+  let a1 = MerageSort(a.slice(0, a.length / 2));
+  let a2 = MerageSort(a.slice(a.length / 2, a.length));
+  let i = 0, j = 0;
   const aSorted = []
-  while(i<a1.length||j<a2.length){
-    if(a1[i]<a2[j] || j>=a2.length){
+  while (i < a1.length || j < a2.length) {
+    if (a1[i] < a2[j] || j >= a2.length) {
       aSorted.push(a1[i]);
       i++;
-    }else{
+    } else {
       aSorted.push(a2[j]);
       j++;
     }
