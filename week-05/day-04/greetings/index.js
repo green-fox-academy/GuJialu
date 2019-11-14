f8412534df2e447a679d2196a12f60199c26b37d
Jialu Gu <msljtacslw@gmail.com>
2019-11-14 15:21:35 +0800

    feat(workshop): add todos

---
+++ week-05/day-04/greetings/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// home page
+app.get('/', (req, res) => {
+  // render `home.ejs`
+  let { name } = req.query;
+  if (name === undefined) {
+    name = 'Guest';
+  }
+  res.render('home', {
+    name: name,
+  });
+});
+
+// start express app on port 3000
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
